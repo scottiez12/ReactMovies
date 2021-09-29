4c3de3cb3db54f9f346e9a1521e97714f9029234
sziegler <sziegler@hcnetwork.org>
2021-09-29 14:06:06 -0400

    Everything working again

--- src/movies/IndividualMovie.tsx
+++ src/movies/IndividualMovie.tsx
@@ -1,10 +1,22 @@
 import { movieDTO } from "./movie.model";
 import css from "./IndividualMovie.module.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "../utilities/Button";
+import customConfirm from "../utilities/customConfirm";
+import axios from "axios";
+import { urlMovies } from "../Endpoints";
+import AlertContext from "../utilities/AlertContext";
 
 export default function IndividualMovie(props: movieDTO) {
   const buildLink = () => `/movie/${props.id}`;
+  const customAlert = useContext(AlertContext);
+
+  function deleteMovie() {
+    axios.delete(`${urlMovies}/${props.id}`).then(() => {
+      customAlert();
+    });
+  }
 
   return (
     <div className={css.div}>
@@ -14,6 +26,21 @@ export default function IndividualMovie(props: movieDTO) {
       <p>
         <Link to={buildLink()}>{props.title}</Link>
       </p>
+      <div>
+        <Link
+          style={{ marginRight: "1rem" }}
+          className="btn btn-info"
+          to={`/movies/edit/${props.id}`}
+        >
+          Edit
+        </Link>
+        <Button
+          onClick={() => customConfirm(() => deleteMovie())}
+          className="btn btn-danger"
+        >
+          Delete
+        </Button>
+      </div>
     </div>
   );
 }
--- src/movies/LandingPage.tsx
+++ src/movies/LandingPage.tsx
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 import { urlMovies } from "../Endpoints";
+import AlertContext from "../utilities/AlertContext";
 import { landingPageDTO } from "./movie.model";
 import MoviesList from "./MoviesList";
 
@@ -8,17 +9,25 @@ export default function LandingPage() {
   const [movies, setMovies] = useState<landingPageDTO>({});
 
   useEffect(() => {
+    loadData();
+  }, []);
+
+  function loadData() {
     axios.get(urlMovies).then((response: AxiosResponse<landingPageDTO>) => {
       setMovies(response.data);
     });
-  }, []);
+  }
 
   return (
-    <>
+    <AlertContext.Provider
+      value={() => {
+        loadData();
+      }}
+    >
       <h3>In Theaters</h3>
       <MoviesList movies={movies.inTheaters} />
       <h3>Upcoming Releases</h3>
       <MoviesList movies={movies.upcomingReleases} />
-    </>
+    </AlertContext.Provider>
   );
 }
